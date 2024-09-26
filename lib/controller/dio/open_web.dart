6246de24b2a740cc0d8739048f086f32097cfd1e
서한유 <hu8232@naver.com>
2024-09-26 17:59:59 +0900

    인스타그램필드변경

--- lib/controller/dio/open_web.dart
+++ lib/controller/dio/open_web.dart
@@ -2,7 +2,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 class OpenWeb {
   Future<void> OpenInstagram(String instagramId) async {
-    String instagramUrl = 'https://www.instagram.com/$instagramId/';
+    String instagramUrl = 'instagram://user?username=$instagramId/';
     if (await canLaunchUrl(Uri.parse(instagramUrl))) {
       await launchUrl(Uri.parse(instagramUrl));
     } else {
