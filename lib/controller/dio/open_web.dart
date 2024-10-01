1931486ff295206a483230a56c35221956eec644
서한유 <hu8232@naver.com>
2024-10-01 19:05:35 +0900

    인스타그램 installd여부 dev에 추가해야됨

--- lib/controller/dio/open_web.dart
+++ lib/controller/dio/open_web.dart
@@ -3,8 +3,11 @@ import 'package:url_launcher/url_launcher.dart';
 class OpenWeb {
   Future<void> OpenInstagram(String instagramId) async {
     String instagramUrl = 'instagram://user?username=$instagramId/';
+    String instagramWebUrl = 'https://www.instagram.com/$instagramId/';
     if (await canLaunchUrl(Uri.parse(instagramUrl))) {
       await launchUrl(Uri.parse(instagramUrl));
+    } else if (await canLaunchUrl(Uri.parse(instagramWebUrl))) {
+      await launchUrl(Uri.parse(instagramWebUrl));
     } else {
       throw 'Could not launch $instagramUrl';
     }
