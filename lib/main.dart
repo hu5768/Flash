ffc8bad7cd3bfb8543dd0a6d8d064092cef8a54b
서한유 <hu8232@naver.com>
2024-10-04 14:31:02 +0900

    firecut

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:flash/firebase_options.dart';
 import 'package:flash/view/main_page.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   runApp(const App());
