ebc5b276baaa0e3c17924dda407d9fb0dfa93c72
서한유 <hu8232@naver.com>
2024-10-04 14:28:15 +0900

    dev

--- lib/const/uploadBaseUrl.dart
+++ lib/const/uploadBaseUrl.dart
@@ -1,3 +1,3 @@
-const uploadServerUrl = 'https://upload.climbing-answer.com/';
+const uploadServerUrl = 'https://upload.dev.climbing-answer.com/';
 //'https://upload.dev.climbing-answer.com/'  개발서버
 //'https://upload.climbing-answer.com/'
