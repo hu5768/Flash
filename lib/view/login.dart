9198719f41ea3db1ffbca219deb90e02182f75c8
서한유 <hu8232@naver.com>
2024-10-05 15:23:24 +0900

    hosting

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -8,8 +8,7 @@ class LoginPage extends StatelessWidget {
   LoginPage({super.key});
   LoginController loginController = LoginController();
   final TextEditingController textController = TextEditingController(
-    text:
-        "ya29.a0AcM612wb8b3LfddauBL1Gtem7_bpqALS-Rwub4kDXdKXF_7jK0V28YdOjVZn76JFHCwgrQdDlTRn5BQ6DP2xoY5WGH9cehkE3YQOEbcRspASXBdmyTCCg5n8dVooxdsN7BjlqS6MUc7Z8HNDfV0Uugzs5oCrDzx2ayIzaLAfaCgYKAZYSARMSFQHGX2MiiG4QZ1SAjb6KmkRP8w3E7Q0175",
+    text: "",
   );
   @override
   Widget build(BuildContext context) {
