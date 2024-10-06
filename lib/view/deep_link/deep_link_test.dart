06996420c374fb98b0970092531d955ade08c9e2
서한유 <hu8232@naver.com>
2024-10-06 23:47:55 +0900

    applinkpub

---
+++ lib/view/deep_link/deep_link_test.dart
@@ -0,0 +1,15 @@
+import 'package:flash/main.dart';
+import 'package:flutter/material.dart';
+
+class DeepLinkTest extends StatelessWidget {
+  const DeepLinkTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('deeplinktest'),
+      ),
+    );
+  }
+}
