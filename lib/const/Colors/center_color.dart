334684826a8f21bd5196cb0add6dab10a9be68e8
서한유 <hu8232@naver.com>
2024-11-02 16:19:32 +0900

    남색추가

--- lib/const/Colors/center_color.dart
+++ lib/const/Colors/center_color.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 class CenterColor {
   static Map<String, Color> TheClimbColorList = {
-    '파랑': Color.fromARGB(248, 58, 58, 254),
+    '파랑': Color.fromARGB(248, 79, 179, 255),
     '빨강': Color.fromARGB(248, 254, 42, 42),
     '보라': Color.fromARGB(248, 152, 53, 251),
     '회색': Color.fromARGB(248, 160, 160, 160),
     '갈색': Color.fromARGB(248, 99, 50, 22),
-    '시크릿': Color.fromARGB(255, 255, 255, 255),
+    '시크릿': Color.fromARGB(255, 0, 0, 0),
+    '이벤트': Color.fromARGB(255, 0, 0, 0),
     '핑크': Color.fromARGB(255, 249, 54, 168),
     '검정': Color.fromARGB(255, 0, 0, 0),
     '흰색': Color.fromARGB(255, 232, 232, 232),
+    '남색': Color.fromARGB(255, 15, 15, 195),
   };
 }
 
